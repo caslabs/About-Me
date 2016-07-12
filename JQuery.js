1aa1c38a412c799c028403459d8892f16476eb19
Jeraldy Cascayan <casourcer@gmail.com>
2016-07-11 23:59:51 -0700

    retest
    added jquery

---
+++ JQuery.js
@@ -0,0 +1,5 @@
+$(document).ready(function(){ 
+    $("#datboi").click(function(){ 
+    $( this ).hide();
+    });
+});
--- javascript.js
+++
@@ -1,3 +0,0 @@
-$(document).ready(function() {
-	$("#wow").css({"left":"2000px"}).animate({"left":"0px"}, "slow");
-})
