55c35e1e2a0c21aa0767a3c275515056c6ac06a4
LimeTire The Retired Alien <Froggyfree86@gmail.com>
2016-09-06 22:01:34 -0400

    Add files via upload

---
+++ quotes.js
@@ -0,0 +1,38 @@
+var inspiration = [
+" Life is about making an impact, not making an income. –Kevin Kruse",
+" Two roads diverged in a wood, and I—I took the one less traveled by, And that has made all the difference.  –Robert Frost",
+" I attribute my success to this: I never gave or took any excuse. –Florence Nightingale",
+"Twenty years from now you will be more disappointed by the things that you didn’t do than by the ones you did do, so throw off the bowlines, sail away from safe harbor, catch the trade winds in your sails.  Explore, Dream, Discover. –Mark Twain",
+"Life is 10% what happens to me and 90% of how I react to it. –Charles Swindoll",
+"Your time is limited, so don’t waste it living someone else’s life. –Steve Jobs",
+"Every child is an artist.  The problem is how to remain an artist once he grows up. –Pablo Picasso",
+"The two most important days in your life are the day you are born and the day you find out why. –Mark Twain",
+"People often say that motivation doesn’t last. Well, neither does bathing.  That’s why we recommend it daily. –Zig Ziglar",
+" Whatever you can do, or dream you can, begin it.  Boldness has genius, power and magic in it. –Johann Wolfgang von Goethe",
+"Either you run the day, or the day runs you. –Jim Rohn"
+]; /*11 quotes*/
+var motivation = ["If you can see the positive sides of everything, you'll be able to live a much richer life than others. - Celestine Chua",
+"Things work out best for those who make the best of how things work out -John Wooden",
+"To live a creative life, we must lose our fear of being wrong",
+"Good things come to people who wait, but better things come to those who go out and get them",
+"Success is walking from failure to failure with no loss of enthusiasm. -Winston Churchhill",
+"Great minds discuss ideas; average minds discuss events; small minds discuss people.",
+"Great minds discuss ideas; average minds discuss events; small minds discuss people.",
+"Too many of us are not living our dreams because we are living our fears. -Les Brown",
+"A real entrepreneur is somebody who has no safety net underneath them -Henry Kravis",
+"People who succeed have momentum. The more they succeed, the more they want to succeed, and the more they find a way to succeed. Similarly, when someone is failing, the tendency is to get on a downward spiral that can even become a self-fulfilling prophecy -Tony Robbins"
+]; /* 10 quotes */
+var funny = [
+"Some people are like clouds. When they go away, it's a brighter day.",
+"When you wake up at 6 in the morning, you close your eyes for 5 minutes and it's already 6:45. When you're at work and it's 2:30, you close your eyes for 5 minutes and it's 2:31.",
+"Candy is dandy but liqueur is quicker. -Willy Wonka",
+"If the good lord intended us to walk,he would have not invented roller skates",
+"Accept who you are. Unless you're a serial killer. -Ellen DeGeneres",
+"If a book about failures doesn't sell, is it a success? -Jerry Seinfeld",
+"A word to the wise ain't necessary, it's the stupid ones who need advice. -Bill Cosby"
+"Age is just a number -Jared Fogle"
+"Everyone playing checkers,But i've been playing checkers for years. -Soflo Antonio",
+"STOP BEING A CENTIPEDE -Alex the Wizard"
+
+]; /*10 quotes */
+
